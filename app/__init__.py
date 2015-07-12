84d5b86bb95c5cfe75d9f55a30d820e3c41507b9
Anthony Carminati <anthony@carminati.io>
2015-07-12 13:16:34 -0500

    v 0.2

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from config import config
+from flask.ext.bootstrap import Bootstrap
+bootstrap = Bootstrap()
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
+
+    bootstrap.init_app(app)
 
     from .analytics import analytics as analytics_blueprint
     app.register_blueprint(analytics_blueprint)
