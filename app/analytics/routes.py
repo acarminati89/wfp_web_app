4c0282d3184c5dd939f86ff2889759e904eb93be
Anthony Carminati <anthony@carminati.io>
2015-07-12 13:08:58 -0500

    server running

--- app/analytics/routes.py
+++ app/analytics/routes.py
@@ -4,3 +4,7 @@ from . import analytics
 @analytics.route('/')
 def index():
     return render_template('analytics/index.html')
+
+@analytics.route('/user/<username>')
+def user(username):
+    return render_template('analytics/user.html')
