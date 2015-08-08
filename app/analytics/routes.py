983e0c654a5aafd775843891e7a0f8fe100c0017
Anthony Carminati <anthony@carminati.io>
2015-08-08 18:28:59 -0400

    may be missing files

--- app/analytics/routes.py
+++ app/analytics/routes.py
@@ -1,11 +1,6 @@
 from flask import render_template
 from . import analytics
 
-@analytics.route('/')#, subdomain='wfp-admin')
-def index():
-    # return "Placeholder for the WFP Management administration page."
-    return render_template('analytics/index.html')
-
-@analytics.route('/user/<username>')#, subdomain='wfp-admin')
-def user(username):
-    return render_template('analytics/user.html')
+@analytics.route('/login')
+def login():
+ return render_template('analytics/login.html')
