3613e56c2bee721e4485c6342f5713a327a43945
Anthony Carminati <anthony@carminati.io>
2015-08-08 18:23:28 -0400

    v 0.3 - added some more to models component - need a way to test

--- manage.py
+++ manage.py
@@ -1,9 +1,26 @@
 import os
 from app import create_app
+from app import db
 from flask.ext.script import Manager
+from app.models import LuUser
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
+
+@manager.command
+def adduser(email, username, admin=False):
+    """Register a new user."""
+    from getpass import getpass
+    password = getpass()
+    password2 = getpass(prompt='Confirm: ')
+    if password != password2:
+        import sys
+        sys.exit('Error: passwords do not match.')
+    db.create_all()
+    user = LuUser(email=email, username=username, password=password, is_admin=admin)
+    db.session.add(user)
+    db.session.commit()
+    print('User {0} was registered successfully.'.format(username))
 
 if __name__ == '__main__':
     manager.run()
