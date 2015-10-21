b6b4e6c9a701335b459cb0b73102c3c263db49d9
Anthony Carminati <anthony@carminati.io>
2015-10-20 21:11:56 -0500

    testing dashboard

--- app/analytics/routes.py
+++ app/analytics/routes.py
@@ -3,4 +3,12 @@ from . import analytics
 
 @analytics.route('/')
 def index():
+ return render_template('analytics/index.html')
+
+@analytics.route('/dashboard')
+def dashboard():
+ return render_template('analytics/dashboard_pure_html.html')
+
+@analytics.route('/db_test')
+def db_test():
  return render_template('analytics/dashboard_pure_html.html')
