9bfa60cade44a3462f2958832ffd6758f454757f
Anthony Carminati <anthony@carminati.io>
2015-11-01 10:13:54 -0600

    fixed login views and removed extra files

---
+++ app/analytics/queries.py
@@ -0,0 +1,37 @@
+sql_grid_overview = '''
+SELECT
+	account
+	,sum(realized)
+FROM fact_daily_pl_report fact
+INNER JOIN dim_calendar_ytd dim ON fact.trade_date = dim.ytd_day
+WHERE dim.day = (select max(trade_date) from fact_daily_pl_report) AND unrealized = 0
+GROUP BY account
+ORDER BY account;
+'''
+
+sql_mtd_pnl = '''
+SELECT
+	sum(realized)::integer
+FROM FACT_DAILY_PL_REPORT fct
+INNER JOIN dim_calendar_mtd dim ON fct.trade_date = dim.mtd_day
+WHERE dim.day = (select max(trade_date) from fact_daily_pl_report)
+	AND unrealized = 0;
+'''
+
+sql_qtd_pnl = '''
+SELECT
+	sum(realized)::integer
+FROM FACT_DAILY_PL_REPORT fct
+INNER JOIN dim_calendar_qtd dim ON fct.trade_date = dim.qtd_day
+WHERE dim.day = CURRENT_DATE
+	AND unrealized = 0;
+'''
+
+sql_ytd_pnl = '''
+SELECT
+	sum(realized)::integer
+FROM FACT_DAILY_PL_REPORT fct
+INNER JOIN dim_calendar_ytd dim ON fct.trade_date = dim.ytd_day
+WHERE dim.day = (select max(trade_date) from fact_daily_pl_report)
+	AND unrealized = 0;
+'''
--- app/analytics/routes.py
+++ app/analytics/routes.py
@@ -1,21 +1,35 @@
 from flask import render_template
 from . import analytics
 from flask.ext.login import login_required
+from app import db
+from queries import *
+import locale
+
+locale.setlocale(locale.LC_ALL, 'en_US')
 
 @analytics.route('/')
 @login_required
 def dashboard():
-    return render_template('analytics/dashboard.html')
+    ytd_pnl = db.engine.execute(sql_ytd_pnl)
+    qtd_pnl = db.engine.execute(sql_qtd_pnl)
+    mtd_pnl = db.engine.execute(sql_mtd_pnl)
+    grid_overview = db.engine.execute(sql_grid_overview)
 
+    for row in ytd_pnl:
+        ytd_pnl = locale.currency(row[0], '$', grouping=True)
 
-@analytics.route('/db_test')
-@login_required
-def db_test():
-    y = 'no variable linked - this is static'
-    return render_template('analytics/db_test.html', y=y)
+    for row in qtd_pnl:
+        qtd_pnl = locale.currency(row[0], '$', grouping=True)
 
+    for row in mtd_pnl:
+        mtd_pnl = locale.currency(row[0], '$', grouping=True)
 
-@analytics.route('/base')
-@login_required
-def base():
-    return render_template('base.html')
+    y = list()
+    for i in grid_overview:
+        y.append((i[0], locale.currency(i[1], '$', grouping=True)))
+
+    return render_template('analytics/dashboard.html'
+                           , ytd_pnl=ytd_pnl
+                           , qtd_pnl=qtd_pnl
+                           , mtd_pnl=mtd_pnl
+                           , y=y)
